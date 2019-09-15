e23148dfd2c442494e199814b78be4fc86eb2d23
Rishabh Singhvi <rdpsinghvi@gmail.com>
2019-09-15 19:13:03 -0400

    testing a square

--- Miestas/miestas/Miestas.cpp
+++ Miestas/miestas/Miestas.cpp
@@ -35,41 +35,53 @@ int main()
 	Window window(config.m_applicationName, config.m_windowWidth, config.m_windowHeight);
 	window.init();
 
-	std::vector<float> vbdata;
+	std::vector<float> vbdata = {
+		0.5f,  0.5f, 0.0f,  // top right
+	 0.5f, -0.5f, 0.0f,  // bottom right
+	-0.5f, -0.5f, 0.0f,  // bottom left
+	-0.5f,  0.5f, 0.0f   // top left 
+	};
 
-	fnoise.SetNoiseType(FastNoise::Perlin);
-	fnoise.SetSeed(10443);
-	constexpr float SCALE_FACTOR = 0.1f;
-	constexpr int MAP_SIZE = 10;
+	std::vector<unsigned int> idata = {
+		0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+	};
 
-	for (int i = 0; i <= MAP_SIZE; i++)
-	{
-		for (int j = 0; j <= MAP_SIZE; j++)
-		{
-			vbdata.push_back(static_cast<float>(j * SCALE_FACTOR));
-			vbdata.push_back(static_cast<float>(fnoise.GetNoise(j, i)));
-			vbdata.push_back(static_cast<float>(i * SCALE_FACTOR));
-		}
-	}
+	//std::vector<float> vbdata;
 
-	std::vector<unsigned int> idata;
-	//unsigned int offset = 0;
-	for (int i = 0; i < MAP_SIZE; i++)
-	{
-		for (int j = 0; j < MAP_SIZE; j++)
-		{
-			unsigned int r1 = i * (MAP_SIZE + 1);
-			unsigned int r2 = (i + 1) * (MAP_SIZE + 1);
+	//fnoise.SetNoiseType(FastNoise::Perlin);
+	//fnoise.SetSeed(10443);
+	//constexpr float SCALE_FACTOR = 0.1f;
+	//constexpr int MAP_SIZE = 10;
 
-			idata.push_back(r1 + j);
-			idata.push_back(r1 + j + 1);
-			idata.push_back(r2 + j + 1);
+	//for (int i = 0; i <= MAP_SIZE; i++)
+	//{
+	//	for (int j = 0; j <= MAP_SIZE; j++)
+	//	{
+	//		vbdata.push_back(static_cast<float>(j * SCALE_FACTOR));
+	//		vbdata.push_back(static_cast<float>(fnoise.GetNoise(j, i)));
+	//		vbdata.push_back(static_cast<float>(i * SCALE_FACTOR));
+	//	}
+	//}
 
-			idata.push_back(r1 + j);
-			idata.push_back(r2 + j + 1);
-			idata.push_back(r2 + j);
-		}
-	}
+	//std::vector<unsigned int> idata;
+	////unsigned int offset = 0;
+	//for (int i = 0; i < MAP_SIZE; i++)
+	//{
+	//	for (int j = 0; j < MAP_SIZE; j++)
+	//	{
+	//		unsigned int r1 = i * (MAP_SIZE + 1);
+	//		unsigned int r2 = (i + 1) * (MAP_SIZE + 1);
+
+	//		idata.push_back(r1 + j);
+	//		idata.push_back(r1 + j + 1);
+	//		idata.push_back(r2 + j + 1);
+
+	//		idata.push_back(r1 + j);
+	//		idata.push_back(r2 + j + 1);
+	//		idata.push_back(r2 + j);
+	//	}
+	//}
 
 
 	auto shader = std::make_unique<Shader>("TriangleTest", "C:\\Users\\rdpsi\\Desktop\\Miestas\\Miestas\\shaders\\TriangleTest.vs", "C:\\Users\\rdpsi\\Desktop\\Miestas\\Miestas\\shaders\\TriangleTest.fs");
@@ -83,7 +95,7 @@ int main()
 	va->bindVertexBuffer(std::move(vbo));
 	va->bindIndexBuffer(std::move(ibo));
 
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	Timer timer;
 
